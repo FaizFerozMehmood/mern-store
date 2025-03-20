f33a6e42c2ba9a7f4e01dc3dd27cc44dbf463639
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2025-03-21 01:28:22 +0500

    Fix Vite deployment issue on Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  build: {
-    target: 'esnext',
-    outDir: 'dist',
-  },
   server: {
-    mimeTypes: {
-      'application/javascript': ['js'],
-    },
-  },
-});
+    origin: 'https://mern-store-seven-eta.vercel.app' 
+  }
+})
