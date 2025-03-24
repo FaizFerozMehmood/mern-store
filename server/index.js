e2e2635870dbeb3c399e4f2e23fbbd199436ea7b
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2025-03-25 00:14:51 +0500

    changes made in backend main file

--- server/index.js
+++ server/index.js
@@ -22,25 +22,25 @@ app.use(express.json());
 //     credentials: true,
 // }))
 
-// app.use(
-//     cors({
-//       // origin: 'http://localhost:5173/',
-//       origin : "https://mern-store-wens.vercel.app",
-//       methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//       allowedHeaders: ['Content-Type', 'Authorization'],
-//     })
-//   );      
-
 app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'https://mern-store-wens.vercel.app' 
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
+    cors({
+      // origin: 'http://localhost:5173/',
+      origin : "https://mern-store-wens.vercel.app",
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+  );      
+
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:5173',
+//       'https://mern-store-wens.vercel.app' 
+//     ],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   })
+// );
 
   app.options('*', cors());
 
