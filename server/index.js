4ad4880b4a0e7a329c6cb7fd1d21fb2f0dcf99b2
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2025-10-02 23:14:50 +0500

    url changed as deployed url

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://mern-store-wens.vercel.app",
     credentials: true,
 }))
 // app.use(
